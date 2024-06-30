64dfb394cffe6660b8394d4b87e73377d425088b
elpulga <phinguyenuit10@gmail.com>
2024-06-30 22:23:16 +0700

    add string

---
+++ string/24/index.js
@@ -0,0 +1,47 @@
+// 415. Add Strings
+
+// Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+// You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+
+// Example 1:
+
+// Input: num1 = "11", num2 = "123"
+// Output: "134"
+// Example 2:
+
+// Input: num1 = "456", num2 = "77"
+// Output: "533"
+// Example 3:
+
+// Input: num1 = "0", num2 = "0"
+// Output: "0"
+
+function addStrings(num1, num2) {
+  let i = num1.length - 1;
+  let j = num2.length - 1;
+  let sum = [];
+  let carry = 0;
+
+  while (i >= 0 || j >= 0 || carry) {
+    let n1 = num1[i] || 0;
+    let n2 = num2[j] || 0;
+
+    let curSum = parseInt(n1) + parseInt(n2) + carry;
+    let reminder = curSum % 10;
+
+    sum.push(reminder);
+
+    carry = curSum >= 10 ? 1 : 0;
+
+    j--;
+    i--;
+  }
+
+  return sum.reverse().join("");
+}
+
+let num1 = "11";
+let num2 = "123";
+
+console.log(addStrings(num1, num2));
---
+++ string/24/note.md
@@ -0,0 +1,27 @@
+- num1 = "11", num2 = "123"
+- i = 1, j = 2
+- sum = []
+- carry = 0
+
+- i = 1 >= 0
+  - n1 = 1, n2 = 3
+  - curSum = 1 + 3 + 0 = 4
+  - reminder = 4 % 10 = 4
+  - sum = [4]
+  - carry = 0
+  - i = 0, j = 1
+- i = 0 >= 0
+  - n1 = 1, n2 = 2
+  - curSum = 1 + 2 + 0 = 3
+  - reminder = 3 % 10 = 3
+  - sum = [4, 3]
+  - carry = 0
+  - i = -1, j = 0
+- j = 0 === 0
+  - n1 = 0, n2 = 1
+  - curSum = 0 + 1 + 0 = 1
+  - reminder = 1 % 10 = 1
+  - sum = [4, 3, 1]
+  - carry = 0
+  - i = -2, j = -1
+    -> return "134"
